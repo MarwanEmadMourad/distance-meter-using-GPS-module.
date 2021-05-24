34d01703cfaba100eb2bac17da23a7b7f572926f
Marwan-Alghandour <73784274+Marwan-Alghandour@users.noreply.github.com>
2021-05-25 00:11:40 +0200

    Add files via upload

---
+++ bluetooth.h
@@ -0,0 +1,5 @@
+#include "TM4C123.h"
+#include <stdint.h>
+#include <stdlib.h>
+void Delay(unsigned long counter); // used to add delay
+void Bluetooth_Write(unsigned char data); // Transmit a character to HC-05 over Tx5 pin 
