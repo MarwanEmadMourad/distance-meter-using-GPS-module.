4e3134457b0095bc334bc24001145e4bb3e6e4a9
Mahmoudnaoum <47036731+Mahmoudnaoum@users.noreply.github.com>
2021-05-25 22:20:28 +0200

    Update longlat.h

--- longlat.h
+++ longlat.h
@@ -9,7 +9,7 @@
 char decimalDegreeslat[10];
 char decimalDegreeslong[10];
 float lat_long[2];
-void DMT_to_decimalDegrees(char slat[] /* input lat DMT */, char slong[] /* input long DMT */, char decimalDegreeslat[] /* output Decimal lat */ , char decimalDegreeslong[] /* output Decimal long */);
+void DMS_to_decimalDegrees(char slat[] /* input lat DMS */, char slong[] /* input long DMS */, char decimalDegreeslat[] /* output Decimal lat */ , char decimalDegreeslong[] /* output Decimal long */);
 
 void GPGLL_to_decimalDegrees( char input[] /* input from the sensor */ , char decimalDegreeslat[] /* output Decimal lat */, char decimalDegreeslong[] /* output Decimal long */ );
 
