de95a8eecd60052d26bb6ae586056b514d05f7ca
Marwan-Alghandour <73784274+Marwan-Alghandour@users.noreply.github.com>
2021-06-08 18:07:36 +0200

    Update pys.py

--- pys.py
+++ pys.py
@@ -29,6 +29,64 @@ while True:
         obj = {"distance": s, "coordinates": points}
         break
 
-with open("file.json", "w") as f:
-    json.dump(obj, f)
-
+with open("index.js", "w") as f:
+    f.write("""let { coordinates, distance } = """+str(obj)+""";
+function initMap() {
+    let i = 0;
+    let j = 1;
+    let mesuredDis = 0;
+    const flightPlanCoordinates = [];
+    flightPlanCoordinates.push(coordinates[0]);
+    while (j != coordinates.length) {
+      mesuredDis =
+        mesuredDis +
+        google.maps.geometry.spherical.computeDistanceBetween(
+          new google.maps.LatLng(coordinates[i].lat, coordinates[i].lng),
+          new google.maps.LatLng(coordinates[j].lat, coordinates[j].lng)
+        );
+      console.log();
+  
+      flightPlanCoordinates.push(coordinates[j]);
+      console.log("flight", flightPlanCoordinates);
+      console.log("cooerdinates", coordinates);
+      console.log("distance", distance);
+      i++;
+      j++;
+    }
+  
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 18,
+      center: coordinates[Math.round(coordinates.length / 2)],
+      mapTypeId: "terrain",
+    });
+    const marker1 = new google.maps.Marker({
+      position: coordinates[0],
+      map: map,
+    });
+    const marker2 = new google.maps.Marker({
+      position: coordinates[coordinates.length - 1],
+      map: map,
+    });
+  
+    const flightPath = new google.maps.Polyline({
+      path: flightPlanCoordinates,
+      geodesic: true,
+      strokeColor: "#FF0000",
+      strokeOpacity: 1.0,
+      strokeWeight: 2,
+    });
+  
+    document.getElementById("tiva").innerHTML = " " + distance.toFixed(2) + " m";
+  
+    document.getElementById("maps").innerHTML =
+      " " + mesuredDis.toFixed(2) + " m";
+  
+    document.getElementById("error").innerHTML =
+      " " +
+      (
+        ((distance.toFixed(2) - mesuredDis.toFixed(2)) / distance.toFixed(2)) *
+        100
+      ).toFixed(2) +
+      " %";
+    flightPath.setMap(map);
+}""")
